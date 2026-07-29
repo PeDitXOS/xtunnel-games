5e2a329ed8e2456c55fad9d382396f1e21fab7ac
peditx <peditx@users.noreply.github.com>
2026-07-29 00:49:34 +0000

    fix: add missing UI dependencies, fix main.tsx, fix implicit any

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { Toaster } from '@/components/ui/toaster'
 import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <TooltipProvider>
-      <App />
-      <Toaster />
-    </TooltipProvider>
-  </BrowserRouter>
+  <TooltipProvider>
+    <App />
+    <Toaster />
+  </TooltipProvider>
 )
